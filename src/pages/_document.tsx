bb1b51a9d6d806867e3edac3637e4605e8f73f21
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2025-12-05 17:53:18 -0400

    Removed prisma tracking to implement google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,43 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from "@next/third-parties/google";
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/x-icon" href="/icon.png" />
-        <meta
-          name="description"
-          content="Welcome to Gregory's Links! Explore my collection of links and discover my various online profiles."
-        />
-        <meta
-          name="keywords"
-          content="Gregory's Links, Links, Profile, Social Media"
-        />
+        <meta name="description" content="Welcome to Gregory's Links! Explore my collection of links and discover my various online profiles." />
+        <meta name="keywords" content="Gregory's Links, Links, Profile, Social Media" />
         <meta name="author" content="Gregory cabral" />
         <meta property="og:title" content="Gregory's Links" />
-        <meta
-          property="og:description"
-          content="Welcome to Gregory's Links! Explore my collection of links and discover my various online profiles."
-        />
+        <meta property="og:description" content="Welcome to Gregory's Links! Explore my collection of links and discover my various online profiles." />
         <meta property="og:image" content="/images/face.png" />
         <meta property="og:url" content="https://links.gregoryc.dev" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Gregory's Links" />
-        <meta
-          name="twitter:description"
-          content="Welcome to Gregory's Links! Explore my collection of links and discover my various online profiles."
-        />
+        <meta name="twitter:description" content="Welcome to Gregory's Links! Explore my collection of links and discover my various online profiles." />
         <meta name="twitter:image" content="https://links.gregoryc.dev" />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        />
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
       </Head>
       <body className="bg-orange-400">
         <Main />
-        <NextScript />
+        <NextScript /> 
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,38 @@
 /* eslint-disable react/no-unescaped-entities */
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { SocialLinks } from '@/data/socialLinks'
-import { LinkCard } from '@/components/LinkCard'
-import { Character } from '../components/Character'
-import { InsertVisit } from '@/db/visits'
-import type { InferGetServerSidePropsType, GetServerSideProps, Metadata } from 'next'
-import { MoreInfo } from '@/components/MoreInfo'
-import Head from 'next/head'
-export const getServerSideProps = (async ({req})=>{
-  const referrer = req.headers.referer;
-  const serverTime = new Date().getTime();
-  await InsertVisit(req.connection.remoteAddress ?? 'unknown',referrer ??"no-referer");
-  return {props:{tracked:true,  serverTime}};
-}) satisfies GetServerSideProps<{tracked:boolean}>
-
-export default function Home({serverTime}: InferGetServerSidePropsType<typeof getServerSideProps>){
+import { SocialLinks } from "@/data/socialLinks";
+import { LinkCard } from "@/components/LinkCard";
+import { Character } from "../components/Character";
+import Head from "next/head";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import { GoogleAnalytics } from "@next/third-parties/google";
+export default function Home() {
   return (
     <>
-     <Head>
+      <Head>
         <title>Gregory's links</title>
       </Head>
-    <div className="w-full ">
-      <div className='fixed right-5 top-5'>
-      <MoreInfo serverTime={serverTime}/>
-      </div>
-      <div className="max-w-lg mt-10 mx-auto p-3">
-        <div className="mx-auto size-72 floating-animation-slow">
-          <Character />
+       <GoogleAnalytics gaId="G-02YKYQF6X2" />
+      <div className="w-full ">
+        <div className="fixed right-5 top-5">
+          <Link href="https://gregoryc.dev/about">
+            <FontAwesomeIcon icon={faInfoCircle} fontSize="35" className="px-5 text-white cursor-pointer" />
+          </Link>
         </div>
-        <h1 className="text-2xl font-bold mb-4 text-center text-white">
-          Gregory Cabral De Los Santos
-        </h1>
-        <p className="text-lg text-white  text-center">
-          A fullstack developer from Dominican Republic that really enjoys to
-          create and learn new things 
-        </p>
-        <div className="mt-8">
-          {SocialLinks.map((link) => (
-            <LinkCard className="mt-4" link={link} key={link.url} />
-          ))}
+        <div className="max-w-lg mt-10 mx-auto p-3">
+          <div className="mx-auto size-72 floating-animation-slow">
+            <Character />
+          </div>
+          <h1 className="text-2xl font-bold mb-4 text-center text-white">Gregory Cabral De Los Santos</h1>
+          <p className="text-lg text-white  text-center">A fullstack developer from Dominican Republic that really enjoys to create and learn new things</p>
+          <div className="mt-8">
+            {SocialLinks.map((link) => (
+              <LinkCard className="mt-4" link={link} key={link.url} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
-
